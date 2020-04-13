74605c0462ecd5c4e0cdf15e25c2af760e006b3f
Sarunas <s.dubinskas@gmail.com>
2020-04-13 13:03:52 +0300

    First commint
    Login, Cart management

---
+++ NUnitTestProject1/DriverFunctions.cs
@@ -0,0 +1,44 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTestProject1
+{
+    public class DriverFunctions
+    {
+        public static void DoClickWhile(IWebDriver driver, string selector)
+        {
+            int iterationCount = 2;
+            do
+            {
+                try
+                {
+                    driver.FindElement(By.CssSelector(selector)).Click();
+                    iterationCount = 0;
+                }
+                catch
+                {
+                    iterationCount--;
+                }
+            } while (iterationCount > 0);
+        }
+        public static void SetWebElementByCssSelector(IWebDriver driver, string selector, ref IWebElement webElement)
+        {
+            webElement = null;
+            int iterationCount = 2;
+            do
+            {
+                try
+                {
+                    webElement = driver.FindElement(By.CssSelector(selector));
+                    iterationCount = 0;
+                }
+                catch
+                {
+                    iterationCount--;
+                }
+            } while (iterationCount > 0);
+        }
+    }
+}
---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        private const string homeUrl = "https://www.kika.lt/";
+        private const string loginName = "s.dubinskas@gmail.com";
+        private const string password = "test123";
+        private const string searchParam = "maistas";
+        private const string mainTitle = "KIKA – žinomiausias gyvūnų prekių parduotuvių tinklas Lietuvoje";
+
+        private IWebElement webElement;
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+        [Test]
+        [Category("Smoke")]
+        public void LoginTest()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--incognito");
+            chromeOptions.AddArgument("--start-maximized");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl(homeUrl);
+
+            Assert.AreEqual(mainTitle, driver.Title);
+
+            DriverFunctions.DoClickWhile(driver, $"#editable_popup[style*='display: block'] .close");
+            DriverFunctions.DoClickWhile(driver, $".need2login");
+
+            DriverFunctions.SetWebElementByCssSelector(driver, $".modal .form-control[name='email']", ref webElement);
+            Assert.NotNull(webElement);
+            webElement.SendKeys(loginName);
+
+            Thread.Sleep(100);
+            DriverFunctions.SetWebElementByCssSelector(driver, $".modal .form-control[name='password']", ref webElement);
+            Assert.NotNull(webElement);
+            webElement.SendKeys(password);
+
+            DriverFunctions.DoClickWhile(driver, $".modal .btn.btn-primary");
+            DriverFunctions.DoClickWhile(driver, $"#editable_popup[style*='display: block'] .close");
+            DriverFunctions.DoClickWhile(driver, $"#profile_menu .title");
+            DriverFunctions.DoClickWhile(driver, $"[href='?logout']");
+
+            Assert.Pass();
+        }
+
+        [Test]
+        public void ProductSearchAddAndRemove()
+        {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.AddArgument("--incognito");
+            chromeOptions.AddArgument("--start-maximized");
+            using IWebDriver driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            driver.Navigate().GoToUrl(homeUrl);
+
+            DriverFunctions.DoClickWhile(driver, $"#editable_popup[style*='display: block'] .close");
+
+            DriverFunctions.DoClickWhile(driver, $".need2login");
+
+            DriverFunctions.SetWebElementByCssSelector(driver, $".modal .form-control[name='email']", ref webElement);
+            Assert.NotNull(webElement);
+            webElement.SendKeys(loginName);
+
+            Thread.Sleep(100);
+            DriverFunctions.SetWebElementByCssSelector(driver, $".modal .form-control[name='password']", ref webElement);
+            Assert.NotNull(webElement);
+            webElement.SendKeys(password);
+
+            DriverFunctions.DoClickWhile(driver, $".modal .btn.btn-primary");
+            DriverFunctions.DoClickWhile(driver, $"#editable_popup[style*='display: block'] .close");
+
+            DriverFunctions.DoClickWhile(driver, $"#quick_search_show");
+            DriverFunctions.SetWebElementByCssSelector(driver, $".form-control[name='search']", ref webElement);
+            Assert.NotNull(webElement);
+            webElement.SendKeys(searchParam);
+            webElement.SendKeys(Keys.Return);
+
+            DriverFunctions.DoClickWhile(driver, $"#logo a");
+            DriverFunctions.DoClickWhile(driver, $"#editable_popup[style*='display: block'] .close");
+
+            DriverFunctions.DoClickWhile(driver, $"#mega_menu .dog a");
+            DriverFunctions.DoClickWhile(driver, $".product_listing .product_element .btn.btn-primary");
+
+            DriverFunctions.DoClickWhile(driver, $"#cart_info a");
+            DriverFunctions.DoClickWhile(driver, $"#cart_items .remove a");
+
+            DriverFunctions.DoClickWhile(driver, $"#profile_menu .title");
+            DriverFunctions.DoClickWhile(driver, $"[href='?logout']");
+
+            Thread.Sleep(5000);
+
+            Assert.Pass();
+        }
+
+        [TearDown]
+        public void End()
+        {
+
+        }
+    }
+}
